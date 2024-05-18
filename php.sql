a1dfb72982a841c6a8e3d7d744a5213b2d83c97d
raldzyy <167946556+raldzyy@users.noreply.github.com>
2024-05-19 01:01:14 +0900

    Add files via upload

---
+++ php.sql
@@ -0,0 +1,8 @@
+USE canteen_order_system;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
